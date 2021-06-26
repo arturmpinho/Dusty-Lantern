ab78124443493ac6c9d8f088e24e20a4fed61720
arturmpinho <arturpinho@gmail.com>
2021-06-26 10:17:24 +0000

    Navigation: Restructured navbar and add CSS

--- dusty_lantern/settings.py
+++ dusty_lantern/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'home'
+    'home',
     # 'allauth.socialaccount.providers.facebook', # check in the end if it has been used
 ]
 
@@ -182,6 +182,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
