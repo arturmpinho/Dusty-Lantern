1c4bf980facf0cafdb2e98e4dfbd3e17a8618045
arturmpinho <arturpinho@gmail.com>
2021-06-30 07:19:29 +0000

    Auctions model: delete unnnecessary fields and run migrations

--- auctions/admin.py
+++ auctions/admin.py
@@ -7,8 +7,10 @@ class AuctionAdmin(admin.ModelAdmin):
         'id',
         'product',
         'base_amount',
-        'number_of_bids',
         'bidding_increment',
         'start_date_time',
         'end_date_time',
     )
+
+
+admin.site.register(Auction, AuctionAdmin)
