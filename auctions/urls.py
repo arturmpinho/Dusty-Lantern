2cb0b4fd0ed53849087adb132ba248a1df2ad640
arturmpinho <arturpinho@gmail.com>
2021-07-03 20:27:45 +0000

    Auctions: Add auction detail functionality

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_auctions, name='auctions'),
+    path('<auction_id>', views.auction_detail, name='auction_detail'),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Auction
 
 
@@ -12,3 +12,15 @@ def all_auctions(request):
     }
 
     return render(request, 'auctions/auctions.html', context)
+
+
+def auction_detail(request, auction_id):
+    """View to return the specific details of an auction"""
+
+    auction = get_object_or_404(Auction, pk=auction_id)
+
+    context = {
+        'auction': auction,
+    }
+
+    return render(request, 'auctions/auction_detail.html', context)
