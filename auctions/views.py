32ebeb4cae4a53b024f304ac8be85fe120c5505a
arturmpinho <arturpinho@gmail.com>
2021-07-05 06:40:34 +0000

    Navigation: Add searching funtionality

--- auctions/views.py
+++ auctions/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (render, redirect,
+                              reverse, get_object_or_404)
+from django.contrib import messages
+from django.db.models import Q
 from .models import Auction
 
 
@@ -6,9 +9,24 @@ def all_auctions(request):
     """View to return the auctions page w/ sorting and search queries"""
 
     auctions = Auction.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please check your searching criteria!")
+                return redirect(reverse('auctions'))
+
+            queries = (
+                        Q(product__title__icontains=query) |
+                        Q(product__description__icontains=query)
+            )
+            auctions = auctions.filter(queries)
 
     context = {
         'auctions': auctions,
+        'search_term': query,
     }
 
     return render(request, 'auctions/auctions.html', context)
