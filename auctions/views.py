c971121b2da57aa30add205aff198b8e80117f69
arturmpinho <arturpinho@gmail.com>
2021-07-05 22:46:35 +0000

    Auctions: Add category filtering functionality

--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import (render, redirect,
 from django.contrib import messages
 from django.db.models import Q
 from .models import Auction
+from products.models import Category
 
 
 def all_auctions(request):
@@ -10,12 +11,19 @@ def all_auctions(request):
 
     auctions = Auction.objects.all()
     query = None
+    category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            auctions = auctions.filter(product__category__name__in=category)
+            category = Category.objects.filter(name__in=category)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Please check your searching criteria!")
+                messages.error(request,
+                               "Please check your searching criteria!")
                 return redirect(reverse('auctions'))
 
             queries = (
@@ -27,6 +35,7 @@ def all_auctions(request):
     context = {
         'auctions': auctions,
         'search_term': query,
+        'category': category,
     }
 
     return render(request, 'auctions/auctions.html', context)
