cc9da86357274efdcfbbb92c7525b988a756a313
arturmpinho <arturpinho@gmail.com>
2021-07-11 20:10:01 +0000

    Auctions: Implement initial countdown timer

--- auctions/static/js/auctions.js
+++ auctions/static/js/auctions.js
@@ -1,11 +1,11 @@
 $('#sort-selector').change(function() {
-    var selector = $(this);
-    var currentUrl = new URL(window.location);
+    let selector = $(this);
+    let currentUrl = new URL(window.location);
 
-    var selectedVal = selector.val();
+    let selectedVal = selector.val();
     if(selectedVal != "reset"){
-        var sort = selectedVal.split(",")[0];
-        var direction = selectedVal.split(",")[1];
+        let sort = selectedVal.split(",")[0];
+        let direction = selectedVal.split(",")[1];
 
         currentUrl.searchParams.set("sort", sort);
         currentUrl.searchParams.set("direction", direction);
@@ -18,3 +18,32 @@ $('#sort-selector').change(function() {
         window.location.replace(currentUrl);
     }
 })
+
+const start_date_time = new Date(document.getElementById('start_date_time').textContent);
+const end_date_time = new Date(document.getElementById('end_date_time').textContent);
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  let timeleft = now - (start_date_time);
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("countdowntimer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (timeleft < 0) {
+    clearInterval(x);
+    document.getElementById("countdowntimer").innerHTML = "EXPIRED";
+  }
+}, 1000);
