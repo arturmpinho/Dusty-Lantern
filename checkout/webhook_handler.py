8fd712781d8212fa46fe76fcfe70fd8a0af4112c
arturmpinho <arturpinho@gmail.com>
2021-07-25 13:13:54 +0000

    Checkout: implement send email functionality

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,8 +4,11 @@ from django.template.loader import render_to_string
 from django.conf import settings
 
 from .models import Order, OrderLineItem
-from auctions.models import Auction, Bid
-# from profiles.models import UserProfile
+from profiles.models import UserProfile
+from auctions.models import Auction, Bid, Bag
+from django.contrib.auth.models import User
+
+from profiles.models import UserProfile
 
 import json
 import time
@@ -16,6 +19,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """
@@ -43,6 +63,20 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        user_profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            user_profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                user_profile.default_phone_number = shipping_details.phone
+                user_profile.default_country = shipping_details.address.country
+                user_profile.default_postcode = shipping_details.address.postal_code
+                user_profile.default_town_or_city = shipping_details.address.city
+                user_profile.default_street_address1 = shipping_details.address.line1
+                user_profile.default_street_address2 = shipping_details.address.line2
+                user_profile.default_county = shipping_details.address.state
+                user_profile.save()
+    
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -67,7 +101,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-            # self._send_confirmation_email(order)
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
                          'Verified order already in database'),
@@ -78,7 +112,7 @@ class StripeWH_Handler:
                 order = Order.objects.create(
                     first_name=shipping_details.name.split(' ')[0],
                     last_name=shipping_details.name.split(' ')[1],
-                    # user_profile=profile,
+                    user_profile=user_profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -105,7 +139,10 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        # self._send_confirmation_email(order)
+        bag = Bag.objects.filter(bidder=user_profile.user)
+        for item in bag:
+            item.delete()
+        self._send_confirmation_email(order)
 
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 from django.contrib import messages
 
 
@@ -30,4 +31,19 @@ def profile(request):
         'orders': orders
     }
     
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'This is a past confirmation for order number {order_number} \
+        A confirmation email was sent on the order date')
+
+    template = "checkout/checkout_success.html"
+
+    context = {
+        'order': order,
+        'from_profile': True
+    }
     return render(request, template, context)
