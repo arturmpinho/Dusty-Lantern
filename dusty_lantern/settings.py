f4c567b7372b7228f59f71ce8630894c4bd32ea9
arturmpinho <arturpinho@gmail.com>
2021-07-28 18:14:51 +0000

    Deployment: Adjust collect static variable

--- dusty_lantern/settings.py
+++ dusty_lantern/settings.py
@@ -178,7 +178,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -206,7 +205,7 @@ if "USE_AWS" in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
