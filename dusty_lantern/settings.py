d4afbf8fb131f4ac668ee084520d9e965d701f83
arturmpinho <arturpinho@gmail.com>
2021-07-28 17:42:04 +0000

    Deployment: update secret key

--- dusty_lantern/settings.py
+++ dusty_lantern/settings.py
@@ -20,7 +20,6 @@ if os.path.exists('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
